cf2707e2be3b0c4c772c9746ed421aa5c2a52588
ThiagoOkamura <thiagokamura14@gmail.com>
2025-11-06 22:28:42 -0500

    SequenceResult class implemented with constructors, getters and a toString method

--- CollatzApp/src/collatzapp/SequenceResult.java
+++ CollatzApp/src/collatzapp/SequenceResult.java
@@ -5,14 +5,55 @@
 package collatzapp;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  *
  * @author thiago
  */
 public class SequenceResult {
-
+    private final int startNum;
+    private final List<Integer> sequence;
+    private final int stepsToOne;
+    private final int peakNum;
+    private final int stopTime;
+    private final double avgGrowth;
+    
     public SequenceResult(int startNum, List<Integer> sequence, int stepsToOne, int peakNum, int stopTime, double avgGrowth) {
-        
+        this.startNum = startNum;
+        this.sequence = sequence;
+        this.stepsToOne = stepsToOne;
+        this.peakNum = peakNum;
+        this.stopTime = stopTime;
+        this.avgGrowth = avgGrowth;
+    }
+
+    @Override
+    public String toString() {
+        return "SequenceResult{" + "startNum=" + startNum + ", sequence=" + sequence + ", stepsToOne=" + stepsToOne + ", peakNum=" + peakNum + ", stopTime=" + stopTime + ", avgGrowth=" + avgGrowth + '}';
+    }    
+
+    public int startNum() { 
+        return startNum; 
+    }
+    
+    public List<Integer> sequence() { 
+        return sequence; 
+    }
+    
+    public int stepsToReachOne() {
+        return stepsToOne;
+    }
+
+    public int totalStoppingTime() { 
+        return stopTime; 
+    }
+    
+    public int peakNum() {
+        return peakNum; 
+    }
+    
+    public double avgGrowth() { 
+        return avgGrowth; 
     }
 }
