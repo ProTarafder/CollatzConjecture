fe0a5ff1ef5d64ac2a7ab48ba1ed37a89329124b
ProTarafder <protititarafder@gmail.com>
2025-11-20 23:48:26 -0500

    FXML commit
    Adding Single Run and Compare Run FXML

---
+++ CollatzApp/CollatzApp/src/collatzapp/CollatzApp.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package collatzapp;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Admin
+ */
+public class CollatzApp extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    public void start(Stage primaryStage){
+        
+    }
+}
