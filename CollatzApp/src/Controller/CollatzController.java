06b976c63ab8e4a55537eacf6f7651ddd11a4379
ProTarafder <protititarafder@gmail.com>
2025-11-21 13:13:41 -0500

    UI Controls + SceneSwitch added

--- CollatzApp/src/Controller/CollatzController.java
+++ CollatzApp/src/Controller/CollatzController.java
@@ -6,20 +6,42 @@ package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * event handler for the project, to be build after scene builder is ready.
+ *
  * @author thiago
  */
 public class CollatzController implements Initializable {
-    
+
+    //UI Controls
+    @FXML
+    private TextField inputField;
+    @FXML
+    private Button startBtn, pauseBtn, resetBtn, exportBtn;
+    @FXML
+    private LineChart<Number, Number> chart;
+    @FXML
+    private NumberAxis xAxis, yAxis;
+    @FXML
+    private TextArea metricsArea;
+
+    @FXML
+    private MenuItem exportMenuItem, exitMenuItem, openCompareMenu, aboutMenu;
+    @FXML
+    private RadioMenuItem linearScaleMenu;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
-    }  
+
+    }
 }
