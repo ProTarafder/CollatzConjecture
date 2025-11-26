02b8d724487c740c2400991cbff9a5bd0e4a8f10
ProTarafder <protititarafder@gmail.com>
2025-11-25 23:42:06 -0500

    LineChart logic and Validation

--- CollatzApp/src/Controller/CollatzController.java
+++ CollatzApp/src/Controller/CollatzController.java
@@ -5,48 +5,76 @@
 package Controller;
 
 import Main.SequenceResult;
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
-import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import Model.CollatzModel;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javafx.animation.Timeline;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.stage.Stage;
 
 /**
  * event handler for the project, to be build after scene builder is ready.
  *
  * @author thiago
  */
-public class CollatzController implements Initializable {
+public class CollatzController{
 
     //UI Controls
-    @FXML
-    private TextField inputField;
-    @FXML
-    private Button startBtn, pauseBtn, resetBtn, exportBtn;
-    @FXML
-    private TextArea metricsArea;
+    @FXML private TextField inputField;
+    @FXML private VBox chartContainer;
+    @FXML private TextArea metricsArea;
+    @FXML private Button startBtn;
+    @FXML private Button pauseBtn;
+    @FXML private Button resetBtn;
+    @FXML private Button exportBtn;
 
-    @FXML
-    private MenuItem exportMenuItem, exitMenuItem, openCompareMenu, aboutMenu;
-    @FXML
-    private RadioMenuItem linearScaleMenu;
-    @FXML
-    private RadioMenuItem logScaleMenu;
-    @FXML
-    private VBox chartContainer;
+    @FXML private MenuItem openCompareMenu;
+    @FXML private MenuItem exportMenuItem;
+    @FXML private MenuItem exitMenuItem;
+    @FXML private MenuItem aboutMenu;
+    
+    private LineChart<Number, Number> chart;
+    
+    private Timeline timeline;
+    private boolean paused = false;
     
     private CollatzModel model = new CollatzModel();
+    private Map<Integer, SequenceResult> memo = new ConcurrentHashMap<>();
+    
+    private static int MAX_RECOMMENDED = 10_000; //can change
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    @FXML
+    public void initialize() {
 
+        NumberAxis x = new NumberAxis();
+        x.setLabel("Step");
+
+        NumberAxis y = new NumberAxis();
+        y.setLabel("Value");
+
+        chart = new LineChart<>(x, y);
+        chart.setAnimated(false);
+        chart.setCreateSymbols(false);
+
+        chartContainer.getChildren().add(chart);
+        
+        openCompareMenu.setOnAction(e -> {
+            try{
+                Stage stage = (Stage) chart.getScene().getWindow();
+                SceneSwitcher.switchTO(stage, "collatz/compareRun.fxml");
+            } catch (Exception ex){
+                ex.printStackTrace();
+            }
+        });
     }
 
     /**
@@ -55,11 +83,22 @@ public class CollatzController implements Initializable {
      */
     @FXML
     private void runOnAction(ActionEvent event) {
-        int num = Integer.parseInt(inputField.getText());
+        List<Integer> seeds = Validation.parseSeeds(inputField.getText());
+        if(seeds.isEmpty()){
+            show("Input required", "Input a positive integer.");
+            return;
+        }
+        int num = seeds.get(0);
+        
+        //validaiton
+        String warning = Validation.validateSeed()
+        
         SequenceResult result = model.calculateSequence(num);
         double avg = result.avgGrowth();
-        String avgFormatted = String.format("%.2f", avg);       
-        metricsArea.setText("Starting number: " + result.startNum() + "\n" +
+        String avgFormatted = String.format("%.2f", avg);    
+        
+        metricsArea.setText(
+                "Starting number: " + result.startNum() + "\n" +
                 "Steps to 1: " + result.stepsToReachOne() + "\n" +
                 "Peak value: " + result.peakNum() + "\n" +
                 "Stopping time (ns): " + result.totalStoppingTime() + "\n" +
--- CollatzApp/src/Controller/CompareController.java
+++ CollatzApp/src/Controller/CompareController.java
@@ -4,23 +4,27 @@
  */
 package Controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
+import Main.SequenceResult;
+import Model.CollatzModel;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
  *
  * @author Admin
  */
-public class CompareController implements Initializable {
+public class CompareController {
 
     @FXML
     private TextField compareInput;
@@ -29,29 +33,75 @@ public class CompareController implements Initializable {
     @FXML
     private VBox compareChartContainer;
     @FXML
-    private LineChart<Number, Number> compareChart;
-    @FXML
     private NumberAxis compareXAxis, compareYAxis;
-
     @FXML
     private TextArea compareMetrics;
+
+    private LineChart<Number, Number> compareChart;
+    private final CollatzModel model = new CollatzModel();
+    private Map<Integer, SequenceResult> memo = new ConcurrentHashMap<>();
 
     /**
      * Initializes the controller class.
      */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        
+    @FXML
+    public void initialize() {
+
         NumberAxis x = new NumberAxis();
         x.setLabel("Step");
-        
+
         NumberAxis y = new NumberAxis();
         y.setLabel("Value");
-        
+
         compareChart = new LineChart<>(x, y);
         compareChart.setAnimated(false);
-        
+        compareChart.setCreateSymbols(false);
         compareChartContainer.getChildren().add(compareChart);
+
+        compareStartBtn.setOnAction(e -> onCompareStart());
+        backBtn.setOnAction(e -> {
+            try {
+                Stage stage = (Stage) compareShart.getScene().getWindow();
+                SceneSwitcher.swithTo(stage, "collatz/SingleRun.fxml");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 
+    private void onCompareStart() {
+        List<Integer> seeds = Validation.parseSeeds(compareInput.getText());
+        if (seeds.isEmpty()) {
+            compareMetrics.setText("No valid seeds provided");
+            return;
+        }
+
+        compareChart.getData().clear();
+        compareMetrics.clear();
+
+        String metricsOutput = "";
+
+        for (int seed : seeds);
+
+        //computes instantly using model
+        SequenceResult result = memo.computeIfAbsent(seed, k -> model.calculateSequence(k));
+
+        //series
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.setName("n =" + seed);
+
+        List<Integer> seq = result.sequence();
+
+        //Timeline to animate plotting
+        compareChart.getData().add(series);
+
+        metricsOutput
+                = metricsOutput
+                + "Seed " + seed + ":\n"
+                + " Steps to 1: " + result.setpsToReachOne() + "\n"
+                + " Sequence length: " + result.sequenceLength() + "\n"
+                + " Peak: " + result.peakNum() + "\n\n";
+    }
+
+    compareMetrics.setText (metricsOutput);
 }
