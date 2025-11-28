f4d56eb378cf9c5cc92dcd10847b753396001e6e
jellospyro <jellospyro@icloud.com>
2025-11-28 12:36:03 -0500

    fixed back button issue for compare mode
    deleted redunded backbtn code.

--- CollatzApp/src/Main/SceneSwitch.java
+++ CollatzApp/src/Main/SceneSwitch.java
@@ -5,21 +5,35 @@
 package Main;
 
 import java.io.IOException;
+import java.net.URL;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- *
- * @author Admin
+ * Utility class for switching scenes in the main stage.
  */
 public class SceneSwitch {
-    private SceneSwitch(){} //Utility class, no instances
+    private SceneSwitch(){} // Utility class, no instances
     
-    //Switches from Stage to new FXML layout (for comapre)
+    /**
+     * Switches the stage to a new FXML layout.
+     * @param stage The current primary stage.
+     * @param fxmlPath The absolute classpath path to the FXML file (e.g., "/View/SingleRun.fxml").
+     * @throws IOException if the FXML file is not found.
+     */
     public static void switchTo(Stage stage, String fxmlPath) throws IOException{
-        FXMLLoader loader = new FXMLLoader(SceneSwitch.class.getResource(fxmlPath));
+        
+        // Explicitly get the resource URL. This is necessary to debug why the location is null.
+        URL location = SceneSwitch.class.getResource(fxmlPath);
+        
+        if (location == null) {
+            // If the URL is null, throw a custom exception with the failing path
+            throw new IOException("FXML file not found on classpath: " + fxmlPath);
+        }
+        
+        FXMLLoader loader = new FXMLLoader(location);
         Parent newRoot = loader.load();
         
         //same window, new root
